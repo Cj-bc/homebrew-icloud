2b154ec03e9b4729d568b6456b8217fc44b06e03
Cj-bc <cj-bc@y-modify.org>
2018-03-24 13:58:28 +0900

    Initial commit

---
+++ icloud.rb
@@ -0,0 +1,13 @@
+class icloud < Formula
+  desc "Simplest CLI interface for icloud"
+  homepage "https://github.com/Cj-bc/icloud"
+  url "https://github.com/Cj-bc/icloud.git"
+  version "0.0.1"
+  sha256 "746fbe224f0157882468b4264ddb851ac673115644ea3b02945fdb13e16d6e4f"
+
+  def install
+    system "mv", "icloud.sh", "icloud"
+    bin.install icloud
+  end
+
+end
